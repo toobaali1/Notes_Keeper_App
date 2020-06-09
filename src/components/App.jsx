723f116885d9cfbb3a235f0604881232bd779c73
Tooba Ali <toobaalined@gmail.com>
2020-06-09 13:11:32 +0500

    Add AddNote functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,29 @@ import CreateArea from './CreateArea';
 function App() {
 	const [ notes, setNotes ] = useState([]);
 
+	const addNote = (title, content) => {
+		// add a note to the notes array
+		const newNote = {
+			title,
+			content
+		};
+
+		setNotes((prevNotes) => {
+			return [ ...prevNotes, newNote ];
+		});
+	};
+
 	return (
 		<div>
 			<Header />
-			<CreateArea />
-			<Note key={1} title="Note title" content="Note content" />
+			<CreateArea onAdd={addNote} />
+
+			{notes.map((note, index) => {
+				return <Note key={index} title={note.title} content={note.content} />;
+			})}
+			{/* <Note key={1} title="Note title" content="Note content" />
 			<Note key={2} title="Note title" content="Note content" />
-			<Note key={3} title="Note title" content="Note content" />
+			<Note key={3} title="Note title" content="Note content" /> */}
 			<Footer />
 		</div>
 	);
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
 
-function CreateArea() {
-  return (
-    <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+function CreateArea(props) {
+	const [ input, setInput ] = useState({
+		title: '',
+		content: ''
+	});
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+
+		setInput((prevValue) => {
+			return {
+				...prevValue,
+				[name]: value
+			};
+		});
+	};
+
+	return (
+		<div>
+			<form
+				onSubmit={(event) => {
+					event.preventDefault();
+				}}
+			>
+				<input name="title" placeholder="Title" onChange={handleChange} value={input.title} />
+				<textarea
+					name="content"
+					placeholder="Take a note..."
+					rows="3"
+					onChange={handleChange}
+					value={input.content}
+				/>
+				<button
+					onClick={() => {
+						props.onAdd(input.title, input.content);
+						setInput({
+							title: '',
+							content: ''
+						});
+					}}
+				>
+					Add
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default CreateArea;
